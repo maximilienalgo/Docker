024e17a68109a5f8b6462ddcf1bb90fe2d6b9e4e
maximilien Pater <70021238+maximilienalgo@users.noreply.github.com>
2021-02-18 09:46:09 +0100

    Add files via upload

--- docker-compose-tp/app/app.py
+++ docker-compose-tp/app/app.py
@@ -1,14 +1,34 @@
 # compose_flask/app.py
 from flask import Flask
-from redis import Redis
+import flask_sqlalchemy
+import sqlalchemy
+import sqlalchemy.dialects.mysql
+
+config = {
+    'host' : 'localhost',
+    'port' : '3320',
+    'user' : 'newuser',
+    'password': 'newpassword',
+    'database' : 'classicmodels'
+}
+
+db_user = config.get('user')
+db_pwd = config.get('password')
+db_host = config.get('host')
+db_port = config.get('port')
+db_name = config.get('database')
 
 app = Flask(__name__)
-redis = Redis(host='redis-container', port=6379)
+
+def create_app():
+    connection_str = 'mysql+mysql://{db_user}:{db_pwd}@{db_host}:3320/{db_name}'
+    engine = sqlalchemy.create_engine(connection_str)
+    connection = engine.connect()
 
 @app.route('/')
-def hello():
-    redis.incr('hits')
-    return ' - - - This basic web page has been viewed {} time(s) - - -'.format(redis.get('hits'))
+def index():
+    return ' - - - MySQL Database `classicmodels` connection ok - - - '
+
 
 
 if __name__ == "__main__":
